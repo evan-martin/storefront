d9e09f42839686db6de58fd8c4df13b1305c3742
evan <evanm689@gmail.com>
2022-07-05 15:15:10 -0700

    responsive item modals

--- src/components/collection-item-modal/collection-item-modal.component.js
+++ src/components/collection-item-modal/collection-item-modal.component.js
@@ -6,15 +6,15 @@ import "./collection-item-modal.styles.scss";
 
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
 import Modal from '@mui/material/Modal';
+import AddIcon from '@mui/icons-material/Add';
+import CloseIcon from '@mui/icons-material/Close';
 
 const style = {
   position: 'absolute',
   top: '50%',
   left: '50%',
   transform: 'translate(-50%, -50%)',
-  width: 400,
   bgcolor: 'background.paper',
   border: '2px solid #000',
   boxShadow: 24,
@@ -54,13 +54,25 @@ const CollectionItemModal = ({ item, addItem }) => {
             <p>
               {description}
             </p>
+
+            <div className="button-bar">
+              <Button
+                startIcon={<AddIcon />}
+                variant="outlined"
+                onClick={() => {
+                  setOpen(false);
+                  addItem(item);
+                }}
+              >Add To Cart</Button>
+              <Button
+                startIcon={<CloseIcon />}
+                variant="outlined"
+                color="secondary"
+                onClick={() => {
+                  setOpen(false);
+                }}>Close </Button>
+            </div>
           </div>
-          <Button
-            onClick={() => {
-              setOpen(false);
-              addItem(item);
-            }}
-          >Add To Cart</Button>
         </Box>
       </Modal>
     </div>
--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
-import { useNavigate } from 'react-router-dom';
 import axios from "axios";
 import CheckoutItem from '../../components/checkout-item/checkout-item.component';
 import { selectCartItems, selectCartTotal } from '../../redux/cart/cart.selectors';
+import Button from '@mui/material/Button';
+import LockIcon from '@mui/icons-material/Lock';
 import './checkout.styles.scss';
 
 const handleClick = ({ cartItems }) => {
@@ -30,11 +31,8 @@ const handleClick = ({ cartItems }) => {
 
 const CheckoutPage = ({ cartItems, total }) => {
 
-  const navigate = useNavigate();
-
   return (
     <div className='checkout-page'>
-      <button onClick={() => navigate(-1)}>Keep Shopping</button>
       <div className='checkout-header'>
         <div className='header-block'>
           <span>Product</span>
@@ -57,10 +55,12 @@ const CheckoutPage = ({ cartItems, total }) => {
       ))}
       <div className='total'>TOTAL: ${total}</div>
       <div className='checkout-button'>
-
-        <button onClick={() => handleClick({ cartItems })}>
+        <Button
+          startIcon={<LockIcon />}
+          variant='contained'
+          onClick={() => handleClick({ cartItems })}>
           Checkout
-        </button>
+        </Button>
 
       </div>
     </div>
