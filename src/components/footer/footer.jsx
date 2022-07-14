5a518a78f50217fc4a86f90a99c0be139fa4e549
evan-martin <evanm689@gmail.com>
2022-07-13 19:11:27 -0700

    add social links to footer

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 import GitHubIcon from '@mui/icons-material/GitHub';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import InfoIcon from '@mui/icons-material/Info';
 
 import "./footer.styles.scss";
 
@@ -8,14 +11,23 @@ export default function Footer() {
     return (
         <div className="footer">
             <div className="logo">
-                <a href="https://github.com/evan-martin/recipe-box" rel="noopener" style={{ textDecoration: "none" }}>
-                    <p>Evan Martin</p>
+                <p>Evan Martin</p>
+                <a href="https://www.evan-martin.dev" target="_blank" rel="noopener noreferrer" style={{ textDecoration: "none" }}>
+                    <AccountCircleIcon />
+                </a>
+                <a href="https://github.com/evan-martin/" target="_blank" rel="noopener noreferrer" style={{ textDecoration: "none" }}>
                     <GitHubIcon />
                 </a>
+                <a href="https://www.linkedin.com/in/evan-martin-2a4ba5225/" target="_blank" rel="noopener noreferrer" style={{ textDecoration: "none" }}>
+                    <LinkedInIcon />
+                </a>
             </div>
+            <div className="logo" >
             <a href="/about" style={{ textDecoration: "none" }} >
                 <p>About</p>
+                <InfoIcon />
             </a>
+            </div>
         </div>
     );
 }
